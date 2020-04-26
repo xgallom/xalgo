449b8fe78b51fe6dc54f0513694ebb80ea3d19fa
Milan Gallo <gallo.milan.jr@gmail.com>
2020-04-26 02:51:11 +0200

    Default initialize GenericType::type to TypeId::Invalid
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- xalgo/include/xalgo/core/GenericType.h
+++ xalgo/include/xalgo/core/GenericType.h
@@ -5,20 +5,22 @@
 #ifndef XALGO_ROOT_XALGO_INCLUDE_XALGO_CORE_GENERICTYPE_H
 #define XALGO_ROOT_XALGO_INCLUDE_XALGO_CORE_GENERICTYPE_H
 
+#include "TypeId.h"
+
 #include <cstddef>
 #include <cstdint>
 
 namespace xalgo::core {
 	struct ConstGenericType {
 		const uint8_t *value = {};
-		size_t type = {};
+		size_t type = TypeId::Invalid;
 
 		explicit operator bool() const noexcept;
 	};
 
 	struct GenericType {
 		uint8_t *value = {};
-		size_t type = {};
+		size_t type = TypeId::Invalid;
 
 		explicit operator bool() const noexcept;
 		operator ConstGenericType() const noexcept;
