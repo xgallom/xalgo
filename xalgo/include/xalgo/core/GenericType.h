77863257db2e6507890823316c804533bb200a69
Milan Gallo <gallo.milan.jr@gmail.com>
2020-04-26 02:45:40 +0200

    Add GenericType
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

---
+++ xalgo/include/xalgo/core/GenericType.h
@@ -0,0 +1,28 @@
+//
+// Created by xgallom on 4/26/20.
+//
+
+#ifndef XALGO_ROOT_XALGO_INCLUDE_XALGO_CORE_GENERICTYPE_H
+#define XALGO_ROOT_XALGO_INCLUDE_XALGO_CORE_GENERICTYPE_H
+
+#include <cstddef>
+#include <cstdint>
+
+namespace xalgo::core {
+	struct ConstGenericType {
+		const uint8_t *value = {};
+		size_t type = {};
+
+		explicit operator bool() const noexcept;
+	};
+
+	struct GenericType {
+		uint8_t *value = {};
+		size_t type = {};
+
+		explicit operator bool() const noexcept;
+		operator ConstGenericType() const noexcept;
+	};
+}
+
+#endif //XALGO_ROOT_XALGO_INCLUDE_XALGO_CORE_GENERICTYPE_H
