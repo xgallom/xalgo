14d0fde7a769ce8e117371d42e78d38a3e355ea2
Milan Gallo <gallo.milan.jr@gmail.com>
2020-04-26 02:45:59 +0200

    Add TypeIndex
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

---
+++ xalgo/include/xalgo/core/TypeIndex.h
@@ -0,0 +1,15 @@
+//
+// Created by xgallom on 4/25/20.
+//
+
+#ifndef XALGO_ROOT_XALGO_INCLUDE_XALGO_CORE_TYPEINDEX_H
+#define XALGO_ROOT_XALGO_INCLUDE_XALGO_CORE_TYPEINDEX_H
+
+namespace xalgo::core {
+	struct TypeIndex {
+		size_t index;
+		size_t type;
+	};
+}
+
+#endif //XALGO_ROOT_XALGO_INCLUDE_XALGO_CORE_TYPEINDEX_H
